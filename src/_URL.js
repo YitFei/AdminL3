2ef60307455536cbe5f77c8596da41db2fbe1f27
Yit Fei <yifei0309@gmail.com>
2022-08-30 00:39:06 +0800

    main

--- src/_URL.js
+++ src/_URL.js
@@ -1,5 +1,5 @@
 // export const Main_URL = "http://localhost:3000";
-
-export const Main_URL = "https://api.l3education.com.my";
+export const Main_URL = "https://admin-l3.vercel.app";
+// export const Main_URL = "https://api.l3education.com.my";
 export const KeyCloak_URL = "http://localhost:3000";
 export const API_URL = "https://api.l3education.com.my";
